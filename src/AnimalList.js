85bc0441226b1c339c5f698a3e2c299e5b48f4f2
MajaNik86 <majanik86@gmail.com>
2022-11-01 21:04:30 +0100

    zadatak3,4, remove animal,place animal to top

--- src/AnimalList.js
+++ src/AnimalList.js
@@ -1,30 +1,33 @@
-import React from "react";
+
+import React, { useState } from "react";
 
 const AnimalList = () => {
-    const animals = [
-        {
+
+
+    const [animals, setAnimal] = useState(
+        [{
             id: 1,
             species: 'Dog',
             name: 'Tara',
-            birth: new Date()
+            birth: new Date().toLocaleTimeString()
         },
         {
             id: 2,
             species: 'Cat',
             name: 'Fluffy',
-            birth: new Date()
+            birth: new Date().toLocaleTimeString()
         },
         {
             id: 3,
             species: 'Rabbit',
             name: 'BugsBunny',
-            birth: new Date()
+            birth: new Date().toLocaleTimeString()
         },
         {
             id: 4,
             species: 'Hampster',
             name: 'Chick',
-            birth: new Date()
+            birth: new Date().toLocaleTimeString()
         },
         {
             id: 5,
@@ -33,7 +36,19 @@ const AnimalList = () => {
             birth: ''
         }
 
-    ]
+        ]);
+
+    const removeHandler = (animalIndex) => {
+        setAnimal([...animals.slice(0, animalIndex), ...animals.slice(animalIndex + 1)]);
+    };
+
+    const handleMoveAnimalToTop = (animalIndex) => {
+        setAnimal([
+            animals[animalIndex],
+            ...animals.slice(0, animalIndex),
+            ...animals.slice(animalIndex + 1)]);
+    }
+
     return (
         <div>
             <h2>List of animals:</h2>
@@ -46,16 +61,19 @@ const AnimalList = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {animals.map((animal) => (
+                    {animals.map((animal, index) => (
                         <tr key={animal.id}>
                             <td>
                                 {animal.species}</td>
                             <td> {animal.name}</td>
                             <td>{animal.birth ? animal.birth.toLocaleString() : 'unknown'}</td>
+                            <td><button onClick={() => { removeHandler(index) }}>Remove</button></td>
+                            <td><button onClick={() => { handleMoveAnimalToTop(index) }}>Top</button></td>
                         </tr>
                     ))}
                 </tbody>
             </table>
+
         </div>
     )
 }
